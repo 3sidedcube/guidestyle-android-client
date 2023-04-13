57195930f50925a81b671eebe94dfec67d77947d
Nikos Rapousis <nikos.rapousis@3sidedcube.com>
2023-04-13 10:29:22 +0100

    Remove CustomFragment

--- app/src/main/java/com/cube/example/MainActivity.kt
+++ app/src/main/java/com/cube/example/MainActivity.kt
@@ -2,11 +2,12 @@ package com.cube.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.cube.styleguide.StyleGuideFragment
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-		CustomStyleGuide().show(supportFragmentManager, "test")
+		StyleGuideFragment().show(supportFragmentManager, StyleGuideFragment::class.java.name)
 	}
 }

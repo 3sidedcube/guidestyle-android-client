922973d4cc00fa89c95e2b37762043c79330b364
Mykhailo Hudov <mike@3sidedcube.com>
2023-10-03 22:27:47 +0100

    - Text style name labels were added to the feed - Horizontal margin was removed from the custom view section

--- library/src/main/java/com/cube/styleguide/adapter/viewholder/TextItemViewHolder.kt
+++ library/src/main/java/com/cube/styleguide/adapter/viewholder/TextItemViewHolder.kt
@@ -5,12 +5,11 @@ import com.cube.styleguide.R
 import com.cube.styleguide.databinding.TextItemViewBinding
 
 class TextItemViewHolder(private val binding: TextItemViewBinding) : RecyclerView.ViewHolder(binding.root) {
-
 	fun populate(pair: Pair<String, Int>) {
-
 		val context = itemView.context
 		binding.apply {
-			headerTitle.text = context.getString(R.string.guidestyle_textstyle_name,pair.first )
+			sectionLabel.text = pair.first
+			headerTitle.text = context.getString(R.string.guidestyle_textstyle_name)
 			headerTitle.setTextAppearance(pair.second)
 		}
 	}
